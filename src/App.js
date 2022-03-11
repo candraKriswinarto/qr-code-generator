4b7d2dab45ea50e05f8d7e495a118bd04dc77349
candra kriswinarto <ckriswinarto@gmail.com>
2022-03-11 16:53:28 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
   }
 
   return (
-    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-96 px-2">
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen pt-24 md:pt-80 px-2">
       <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
         <div className="md:grid md:grid-cols-3">
           <InputContext.Provider value={value}>
